25e196b65e21c25aa3b26c7c749dc06d9436cd79
Muhammad Ovi <muhammadowaisnaseem@gmail.com>
2019-05-16 06:08:06 +0500

    Added option to add thumbnail

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { Image, Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 import Modal from './Modal'
 import PropTypes from 'prop-types';
 
 class Images extends Component {
   static defaultProps = {
-    images: [],
     hideOverlay: false,
     renderOverlay: () => 'Preview Image',
     overlayBackgroundColor: '#222222',
@@ -19,7 +18,9 @@ class Images extends Component {
     this.state = {
       modal: false,
       countFrom: props.countFrom > 0 && props.countFrom < 5 ? props.countFrom : 5,
-      conditionalRender: false
+      conditionalRender: false,
+      imageUrls: [],
+      thumbnails: [],
     };
 
     this.openModal = this.openModal.bind(this);
@@ -30,28 +31,57 @@ class Images extends Component {
     }
   }
 
+  componentDidMount() {
+    const {images} = this.props;
+    if(!images.length) return;
+
+    const imageUrls = [];
+    const thumbnails = [];
+
+    images.forEach(img => {
+      if(this.pureTypeOf(img) === 'object') {
+        imageUrls.push(img.url);
+        thumbnails.push(img.thumbnail || img.url);
+      } else {
+        imageUrls.push(img);
+        thumbnails.push(img);
+      }
+
+      this.setState({ imageUrls, thumbnails })
+    })
+  }
+  
+
   openModal(index) {
-    const {onClickEach, images} = this.props;
+    const {onClickEach} = this.props;
+    const {imageUrls} = this.state;
 
     if(onClickEach) {
-      return onClickEach({src: images[index], index})
+      return onClickEach({src: imageUrls[index], index})
     }
 
-    this.setState({modal: true, url: images[index], index})
+    this.setState({modal: true, url: imageUrls[index], index})
   }
 
   onClose() {
     this.setState({modal: false})
   }
 
+  pureTypeOf = obj => {
+    return Object.prototype.toString
+      .call(obj)
+      .slice(8, -1)
+      .toLowerCase();
+  };
+
   renderOne() {
     const {images} = this.props;
-    const {countFrom} = this.state;
+    const {countFrom, thumbnails} = this.state;
     const overlay = images.length > countFrom && countFrom == 1 ? this.renderCountOverlay(true) : this.renderOverlay();
 
     return  <Grid>
       <Row>
-        <Col xs={12} md={12} className={`border height-one background`} onClick={this.openModal.bind(this, 0)} style={{background: `url(${images[0]})`}}>
+        <Col xs={12} md={12} className={`border height-one background`} onClick={this.openModal.bind(this, 0)} style={{background: `url(${thumbnails[0]})`}}>
           {overlay}
         </Col>
       </Row>
@@ -60,16 +90,16 @@ class Images extends Component {
 
   renderTwo() {
     const {images} = this.props;
-    const {countFrom} = this.state;
+    const {countFrom, thumbnails} = this.state;
     const overlay = images.length > countFrom && [2, 3].includes(+countFrom) ? this.renderCountOverlay(true) : this.renderOverlay();
     const conditionalRender = [3, 4].includes(images.length) || images.length > +countFrom && [3, 4].includes(+countFrom);
 
     return <Grid>
       <Row>
-        <Col xs={6} md={6} className="border height-two background" onClick={this.openModal.bind(this, conditionalRender ? 1 : 0)} style={{background: `url(${conditionalRender ? images[1] : images[0]})`}}>
+        <Col xs={6} md={6} className="border height-two background" onClick={this.openModal.bind(this, conditionalRender ? 1 : 0)} style={{background: `url(${conditionalRender ? thumbnails[1] : thumbnails[0]})`}}>
           {this.renderOverlay()}
         </Col>
-        <Col xs={6} md={6} className="border height-two background" onClick={this.openModal.bind(this, conditionalRender ? 2 : 1)} style={{background: `url(${conditionalRender ? images[2] : images[1]})`}}>
+        <Col xs={6} md={6} className="border height-two background" onClick={this.openModal.bind(this, conditionalRender ? 2 : 1)} style={{background: `url(${conditionalRender ? thumbnails[2] : thumbnails[1]})`}}>
           {overlay}
         </Col>
       </Row>
@@ -78,19 +108,19 @@ class Images extends Component {
 
   renderThree(more) {
     const {images} = this.props;
-    const {countFrom} = this.state;
+    const {countFrom, thumbnails} = this.state;
     const overlay = !countFrom || countFrom > 5 || images.length > countFrom && [4, 5].includes(+countFrom) ? this.renderCountOverlay(true) : this.renderOverlay(conditionalRender ? 3 : 4);
     const conditionalRender = images.length == 4 || images.length > +countFrom && +countFrom == 4;
 
     return <Grid>
       <Row>
-        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 1 : 2)} style={{background: `url(${conditionalRender ? images[1] : images[2]})`}}>
+        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 1 : 2)} style={{background: `url(${conditionalRender ? thumbnails[1] : thumbnails[2]})`}}>
           {this.renderOverlay(conditionalRender ? 1 : 2)}
         </Col>
-        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 2 : 3)} style={{background: `url(${conditionalRender ? images[2] : images[3]})`}}>
+        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 2 : 3)} style={{background: `url(${conditionalRender ? thumbnails[2] : thumbnails[3]})`}}>
           {this.renderOverlay(conditionalRender ? 2 : 3)}
         </Col>
-        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 3 : 4)} style={{background: `url(${conditionalRender ? images[3] : images[4]})`}}>
+        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 3 : 4)} style={{background: `url(${conditionalRender ? thumbnails[3] : thumbnails[4]})`}}>
           {overlay}
         </Col>
       </Row>
@@ -106,7 +136,7 @@ class Images extends Component {
 
     return [
       <div key={`cover-${id}`} className="cover slide" style={{backgroundColor: overlayBackgroundColor}}></div>,
-      <div key={`cover-text-${id}`} className="cover-text slide animate-text"  style={{fontSize: '100%'}}>
+      <div key={`cover-text-${id}`} className="cover-text slide animate-text" style={{fontSize: '100%'}}>
         {renderOverlay()}
       </div>
     ]
@@ -121,7 +151,7 @@ class Images extends Component {
   }
 
   render(){
-    const {modal, index, countFrom} = this.state;
+    const {modal, index, countFrom, imageUrls} = this.state;
     const {images} = this.props;
     const imagesToShow = [...images];
 
@@ -135,7 +165,7 @@ class Images extends Component {
           {imagesToShow.length >= 2 && imagesToShow.length != 4 && this.renderTwo()}
           {imagesToShow.length >= 4 && this.renderThree()}
 
-          {modal && <Modal onClose={this.onClose} index={index} images={images}/>}
+          {modal && <Modal onClose={this.onClose} index={index} images={imageUrls} />}
         </div>
     )
   }
@@ -143,7 +173,10 @@ class Images extends Component {
 }
 
 Images.propTypes = {
-  images: PropTypes.array.isRequired,
+  images: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.arrayOf(PropTypes.shape({ url: PropTypes.string.isRequired })),
+  ]).isRequired,
   hideOverlay: PropTypes.bool,
   renderOverlay: PropTypes.func,
   overlayBackgroundColor: PropTypes.string,
