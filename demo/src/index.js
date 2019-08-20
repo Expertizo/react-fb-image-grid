231eef120dce1d506f4181742a8589335bedafbd
hams ahmed ansari <hams.ahmed.ansari95@gmail.com>
2019-08-20 02:14:38 -0700

    Add Support to Video and Iframe's

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,39 +1,68 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
-import '../../src/css/style.css';
-import FbImageLibrary from '../../src'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "../../src/css/style.css";
+import FbImageLibrary from "../../src";
 
 const images = [
-  'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350',
-  'https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
-  'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350',
-  'https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg',
-  'https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&h=350',
-  'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350',
-  'https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1000'
-]
+  "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350",
+  ,
+  // "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
+  // "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350",
+  // "https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg",
+  // "https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&h=350",
+  // "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350",
+  // "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1000",
+  () => (
+    <iframe
+      title="cats"
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/5dsGWM5XGdg"
+      style={{
+        maxWidth: "97%",
+        position: "absolute",
+        left: 0,
+        right: 0,
+        margin: "auto",
+        top: "50%",
+        transform: "translateY(-50%)"
+      }}
+    />
+  )
+];
 
 const imgs = [
   {
-    url: 'https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg',
-    thumbnail: 'https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1000'
+    url:
+      "https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg",
+    thumbnail:
+      "https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1000"
   },
   {
-    url: 'https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg'
+    url: "https://www.youtube.com/embed/5dsGWM5XGdg",
+    iFrame: true,
+    props: {},
+    thumbnail:
+      "https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg"
   },
   {
-    url: 'https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg'
+    isVideo: true,
+    url: "http://techslides.com/demos/sample-videos/small.webm",
+    props: {},
+    thumbnail:
+      "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"
   }
-]
+];
 
 class Demo extends Component {
   render() {
     return (
       <div>
         {/* images can be 'images' or 'imgs' */}
-        <FbImageLibrary images={images} />
-      </div>)
-    }
+        <FbImageLibrary images={imgs} />
+      </div>
+    );
+  }
 }
 
-render(<Demo />, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,70 +1,116 @@
-import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
-import Modal from './Modal'
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { Grid, Row, Col } from "react-bootstrap";
+import Modal from "./Modal";
+import PropTypes from "prop-types";
 
 class Images extends Component {
   static defaultProps = {
     hideOverlay: false,
-    renderOverlay: () => 'Preview Image',
-    overlayBackgroundColor: '#222222',
+    renderOverlay: () => "Preview Image",
+    overlayBackgroundColor: "#222222",
     onClickEach: null,
     countFrom: 5
-  }
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       modal: false,
-      countFrom: props.countFrom > 0 && props.countFrom < 5 ? props.countFrom : 5,
+      countFrom:
+        props.countFrom > 0 && props.countFrom < 5 ? props.countFrom : 5,
       conditionalRender: false,
       imageUrls: [],
-      thumbnails: [],
+      selectedImage: {},
+      thumbnails: []
     };
 
     this.openModal = this.openModal.bind(this);
     this.onClose = this.onClose.bind(this);
 
-    if(props.countFrom <= 0 || props.countFrom > 5) {
-      console.warn('countFrom is limited to 5!')
+    if (props.countFrom <= 0 || props.countFrom > 5) {
+      console.warn("countFrom is limited to 5!");
     }
   }
 
   componentDidMount() {
-    const {images} = this.props;
-    if(!images.length) return;
+    const { images } = this.props;
+    if (!images.length) return;
 
     const imageUrls = [];
     const thumbnails = [];
 
     images.forEach(img => {
-      if(this.pureTypeOf(img) === 'object') {
-        imageUrls.push(img.url);
-        thumbnails.push(img.thumbnail || img.url);
+      if (this.pureTypeOf(img) === "object") {
+        if ("iFrame" in img) {
+          imageUrls.push(() => (
+            <iframe
+              title="cats"
+              width="560"
+              height="315"
+              src={img.url}
+              style={{
+                maxWidth: "97%",
+                position: "absolute",
+                left: 0,
+                right: 0,
+                margin: "auto",
+                top: "50%",
+                transform: "translateY(-50%)"
+              }}
+              {...img.props}
+            />
+          ));
+          thumbnails.push(img.thumbnail || null);
+        } else if ("isVideo" in img && img.isVideo) {
+          imageUrls.push(() => (
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <video width="400" controls source={img.url} {...img.props}>
+                <source src={img.url} />
+              </video>
+            </div>
+          ));
+          thumbnails.push(img.thumbnail || null);
+        } else {
+          imageUrls.push(img.url);
+          thumbnails.push(img.thumbnail || img.url);
+        }
       } else {
         imageUrls.push(img);
         thumbnails.push(img);
       }
 
-      this.setState({ imageUrls, thumbnails })
-    })
+      this.setState({ imageUrls, thumbnails });
+    });
   }
-  
 
   openModal(index) {
-    const {onClickEach} = this.props;
-    const {imageUrls} = this.state;
+    const { onClickEach, images } = this.props;
+    const { imageUrls } = this.state;
 
-    if(onClickEach) {
-      return onClickEach({src: imageUrls[index], index})
+    if (onClickEach) {
+      return onClickEach({ src: imageUrls[index], index });
     }
 
-    this.setState({modal: true, url: imageUrls[index], index})
+    this.setState({
+      modal: true,
+      url: imageUrls[index],
+      index,
+      selectedImage: images[index]
+    });
   }
 
   onClose() {
-    this.setState({modal: false})
+    this.setState({ modal: false });
   }
 
   pureTypeOf = obj => {
@@ -75,113 +121,217 @@ class Images extends Component {
   };
 
   renderOne() {
-    const {images} = this.props;
-    const {countFrom, thumbnails} = this.state;
-    const overlay = images.length > countFrom && countFrom == 1 ? this.renderCountOverlay(true) : this.renderOverlay();
+    const { images } = this.props;
+    const { countFrom, thumbnails } = this.state;
+    const overlay =
+      images.length > countFrom && countFrom == 1
+        ? this.renderCountOverlay(true)
+        : this.renderOverlay();
 
-    return  <Grid>
-      <Row>
-        <Col xs={12} md={12} className={`border height-one background`} onClick={this.openModal.bind(this, 0)} style={{background: `url(${thumbnails[0]})`}}>
-          {overlay}
-        </Col>
-      </Row>
-    </Grid>;
+    return (
+      <Grid>
+        <Row>
+          <Col
+            xs={12}
+            md={12}
+            className={`border height-one background`}
+            onClick={this.openModal.bind(this, 0)}
+            style={{ background: `url(${thumbnails[0]})` }}
+          >
+            {overlay}
+          </Col>
+        </Row>
+      </Grid>
+    );
   }
 
   renderTwo() {
-    const {images} = this.props;
-    const {countFrom, thumbnails} = this.state;
-    const overlay = images.length > countFrom && [2, 3].includes(+countFrom) ? this.renderCountOverlay(true) : this.renderOverlay();
-    const conditionalRender = [3, 4].includes(images.length) || images.length > +countFrom && [3, 4].includes(+countFrom);
+    const { images } = this.props;
+    const { countFrom, thumbnails } = this.state;
+    const overlay =
+      images.length > countFrom && [2, 3].includes(+countFrom)
+        ? this.renderCountOverlay(true)
+        : this.renderOverlay();
+    const conditionalRender =
+      [3, 4].includes(images.length) ||
+      (images.length > +countFrom && [3, 4].includes(+countFrom));
 
-    return <Grid>
-      <Row>
-        <Col xs={6} md={6} className="border height-two background" onClick={this.openModal.bind(this, conditionalRender ? 1 : 0)} style={{background: `url(${conditionalRender ? thumbnails[1] : thumbnails[0]})`}}>
-          {this.renderOverlay()}
-        </Col>
-        <Col xs={6} md={6} className="border height-two background" onClick={this.openModal.bind(this, conditionalRender ? 2 : 1)} style={{background: `url(${conditionalRender ? thumbnails[2] : thumbnails[1]})`}}>
-          {overlay}
-        </Col>
-      </Row>
-    </Grid>;
+    return (
+      <Grid>
+        <Row>
+          <Col
+            xs={6}
+            md={6}
+            className="border height-two background"
+            onClick={this.openModal.bind(this, conditionalRender ? 1 : 0)}
+            style={{
+              background: `url(${
+                conditionalRender ? thumbnails[1] : thumbnails[0]
+              })`
+            }}
+          >
+            {this.renderOverlay()}
+          </Col>
+          <Col
+            xs={6}
+            md={6}
+            className="border height-two background"
+            onClick={this.openModal.bind(this, conditionalRender ? 2 : 1)}
+            style={{
+              background: `url(${
+                conditionalRender ? thumbnails[2] : thumbnails[1]
+              })`
+            }}
+          >
+            {overlay}
+          </Col>
+        </Row>
+      </Grid>
+    );
   }
 
   renderThree(more) {
-    const {images} = this.props;
-    const {countFrom, thumbnails} = this.state;
-    const overlay = !countFrom || countFrom > 5 || images.length > countFrom && [4, 5].includes(+countFrom) ? this.renderCountOverlay(true) : this.renderOverlay(conditionalRender ? 3 : 4);
-    const conditionalRender = images.length == 4 || images.length > +countFrom && +countFrom == 4;
+    const { images } = this.props;
+    const { countFrom, thumbnails } = this.state;
+    const overlay =
+      !countFrom ||
+      countFrom > 5 ||
+      (images.length > countFrom && [4, 5].includes(+countFrom))
+        ? this.renderCountOverlay(true)
+        : this.renderOverlay(conditionalRender ? 3 : 4);
+    const conditionalRender =
+      images.length == 4 || (images.length > +countFrom && +countFrom == 4);
 
-    return <Grid>
-      <Row>
-        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 1 : 2)} style={{background: `url(${conditionalRender ? thumbnails[1] : thumbnails[2]})`}}>
-          {this.renderOverlay(conditionalRender ? 1 : 2)}
-        </Col>
-        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 2 : 3)} style={{background: `url(${conditionalRender ? thumbnails[2] : thumbnails[3]})`}}>
-          {this.renderOverlay(conditionalRender ? 2 : 3)}
-        </Col>
-        <Col xs={6} md={4} className="border height-three background" onClick={this.openModal.bind(this, conditionalRender ? 3 : 4)} style={{background: `url(${conditionalRender ? thumbnails[3] : thumbnails[4]})`}}>
-          {overlay}
-        </Col>
-      </Row>
-    </Grid>;
+    return (
+      <Grid>
+        <Row>
+          <Col
+            xs={6}
+            md={4}
+            className="border height-three background"
+            onClick={this.openModal.bind(this, conditionalRender ? 1 : 2)}
+            style={{
+              background: `url(${
+                conditionalRender ? thumbnails[1] : thumbnails[2]
+              })`
+            }}
+          >
+            {this.renderOverlay(conditionalRender ? 1 : 2)}
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            className="border height-three background"
+            onClick={this.openModal.bind(this, conditionalRender ? 2 : 3)}
+            style={{
+              background: `url(${
+                conditionalRender ? thumbnails[2] : thumbnails[3]
+              })`
+            }}
+          >
+            {this.renderOverlay(conditionalRender ? 2 : 3)}
+          </Col>
+          <Col
+            xs={6}
+            md={4}
+            className="border height-three background"
+            onClick={this.openModal.bind(this, conditionalRender ? 3 : 4)}
+            style={{
+              background: `url(${
+                conditionalRender ? thumbnails[3] : thumbnails[4]
+              })`
+            }}
+          >
+            {overlay}
+          </Col>
+        </Row>
+      </Grid>
+    );
   }
 
   renderOverlay(id) {
-    const {hideOverlay, renderOverlay, overlayBackgroundColor} = this.props;
+    const { hideOverlay, renderOverlay, overlayBackgroundColor } = this.props;
 
-    if(hideOverlay) {
-      return false
+    if (hideOverlay) {
+      return false;
     }
 
     return [
-      <div key={`cover-${id}`} className="cover slide" style={{backgroundColor: overlayBackgroundColor}}></div>,
-      <div key={`cover-text-${id}`} className="cover-text slide animate-text" style={{fontSize: '100%'}}>
+      <div
+        key={`cover-${id}`}
+        className="cover slide"
+        style={{ backgroundColor: overlayBackgroundColor }}
+      />,
+      <div
+        key={`cover-text-${id}`}
+        className="cover-text slide animate-text"
+        style={{ fontSize: "100%" }}
+      >
         {renderOverlay()}
       </div>
-    ]
+    ];
   }
 
   renderCountOverlay(more) {
-    const {images} = this.props;
-    const {countFrom} = this.state;
+    const { images } = this.props;
+    const { countFrom } = this.state;
     const extra = images.length - (countFrom && countFrom > 5 ? 5 : countFrom);
 
-    return [more && <div key="count" className="cover"></div>, more && <div key="count-sub" className="cover-text" style={{fontSize: '200%'}}><p>+{extra}</p></div>]
+    return [
+      more && <div key="count" className="cover" />,
+      more && (
+        <div
+          key="count-sub"
+          className="cover-text"
+          style={{ fontSize: "200%" }}
+        >
+          <p>+{extra}</p>
+        </div>
+      )
+    ];
   }
 
-  render(){
-    const {modal, index, countFrom, imageUrls} = this.state;
-    const {images} = this.props;
+  render() {
+    const { modal, index, countFrom, imageUrls, selectedImage } = this.state;
+    const { images } = this.props;
     const imagesToShow = [...images];
 
-    if(countFrom && images.length > countFrom) {
+    if (countFrom && images.length > countFrom) {
       imagesToShow.length = countFrom;
     }
 
-    return(
-        <div className="grid-container">
-          {[1, 3, 4].includes(imagesToShow.length)  && this.renderOne()}
-          {imagesToShow.length >= 2 && imagesToShow.length != 4 && this.renderTwo()}
-          {imagesToShow.length >= 4 && this.renderThree()}
+    return (
+      <div className="grid-container">
+        {[1, 3, 4].includes(imagesToShow.length) && this.renderOne()}
+        {imagesToShow.length >= 2 &&
+          imagesToShow.length != 4 &&
+          this.renderTwo()}
+        {imagesToShow.length >= 4 && this.renderThree()}
 
-          {modal && <Modal onClose={this.onClose} index={index} images={imageUrls} />}
-        </div>
-    )
+        {modal && (
+          <Modal
+            onClose={this.onClose}
+            index={index}
+            images={imageUrls}
+            allImages={this.props.images}
+            selectedImage={selectedImage}
+          />
+        )}
+      </div>
+    );
   }
-
 }
 
 Images.propTypes = {
   images: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.string),
-    PropTypes.arrayOf(PropTypes.shape({ url: PropTypes.string.isRequired })),
+    PropTypes.arrayOf(PropTypes.shape({ url: PropTypes.string.isRequired }))
   ]).isRequired,
   hideOverlay: PropTypes.bool,
   renderOverlay: PropTypes.func,
   overlayBackgroundColor: PropTypes.string,
   onClickEach: PropTypes.func,
-  countFrom: PropTypes.number,
+  countFrom: PropTypes.number
 };
 
 export default Images;
