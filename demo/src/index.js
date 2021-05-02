7ad4b5368ddc734eebde15f5ca794a7ac8ce3e14
ManiloxGT <60653867+ManelGonzalez-ops@users.noreply.github.com>
2021-05-02 15:16:02 +0200

    removed react-bootstrap and css Bootrstap import and used Material-ui's grid

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,7 +5,7 @@ import FbImageLibrary from '../../src'
 
 const images = ['https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350',
     'https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg',
-    'https://wallpaperbrowse.com/media/images/soap-bubble-1958650_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg',
     'https://cdn.pixabay.com/photo/2016/10/27/22/53/heart-1776746_960_720.jpg',
     'https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&h=350',
     "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=350",
@@ -13,8 +13,10 @@ const images = ['https://images.pexels.com/photos/248797/pexels-photo-248797.jpe
 
 class Demo extends Component {
     render() {
-        return <div>
-            <FbImageLibrary images={images}/>
+        return <div style={{width: "500px"}}>
+            <FbImageLibrary images={images}
+            
+            />
         </div>
     }
 }
